fd24f786664ed5b315eff88f5f7c96c3d7bf52fe
Nikita Sapronov <sapronov-na@ddsfirm.ru>
2019-02-07 02:27:46 +0500

    lesson 2

---
+++ lesson 2/Person.java
@@ -0,0 +1,33 @@
+public class Person {
+	
+	String gender = "man";
+	String name = "Ivan";
+	float height = 1.80;
+	float weight = 81.50;
+	int age = 25;
+
+	void go() {
+
+	}
+
+	void sit() {
+
+	}
+
+	void run() {
+
+	}
+
+	void sit() {
+
+	}
+
+	void talk() {
+
+	}
+
+	void learnJava() {
+
+	}
+
+}
