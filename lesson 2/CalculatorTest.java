c85a2066b704b1b5c984bf860a23ff9a00391a2f
Nikita Sapronov <sapronov-na@ddsfirm.ru>
2019-02-15 01:29:29 +0500

    update calculatorTest

--- lesson 2/CalculatorTest.java
+++ lesson 2/CalculatorTest.java
@@ -4,32 +4,28 @@ public class CalculatorTest {
 	
 	public static void main(String[] args) {
 		Calculator myCalculator = new Calculator();
+		String answer;
 
-		System.out.println("Enter the first number: ");
-		Scanner firstNumber = new Scanner(System.in);
-		myCalculator.setNumber1(firstNumber.nextInt());
+		do {
+			System.out.println("Enter the first number: ");
+			Scanner firstNumber = new Scanner(System.in);
+			myCalculator.setNumber1(firstNumber.nextInt());
 
-		System.out.println("Enter the sign of the mathematical operation: ");
-		Scanner inputOperation = new Scanner(System.in);
-		myCalculator.setOperation(inputOperation.next().charAt(0));
+			System.out.println("Enter the sign of the mathematical operation: ");
+			Scanner inputOperation = new Scanner(System.in);
+			myCalculator.setOperation(inputOperation.next().charAt(0));
 
-		System.out.println("Enter the second number: ");
-		Scanner secondNumber = new Scanner(System.in);
-		myCalculator.setNumber2(secondNumber.nextInt());
+			System.out.println("Enter the second number: ");
+			Scanner secondNumber = new Scanner(System.in);
+			myCalculator.setNumber2(secondNumber.nextInt());
+			System.out.println("Total: " + myCalculator.getResult());
 
-		System.out.println("Total: " + myCalculator.getResult());
+			do {
+				System.out.println("Want to continue? [Yes/No]");
+				Scanner finish = new Scanner(System.in);
+				answer = finish.nextLine();
+			} while(!answer.equals("Yes") && !answer.equals("No"));
 
-		System.out.println("Want to continue? [Yes/No]");
-		Scanner finish = new Scanner(System.in);
-		String answer = finish.nextLine();
-		
-		if(answer.equals("Yes")) {
-			System.out.println("Try again!");	
-		} else if(answer.equals("No")) {
-			System.out.println("GoodBye!");
-		} else {
-			System.out.println("Want to continue? [Yes/No]");
-			finish.nextLine();
-		}
+		} while(answer.equals("Yes"));
 	}
 }
