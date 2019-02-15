8bf50d71811729834c3e61f5396ec3878bc90f6e
Nikita Sapronov <sapronov-na@ddsfirm.ru>
2019-02-15 16:54:09 +0500

    up calc

--- lesson 2/CalculatorTest.java
+++ lesson 2/CalculatorTest.java
@@ -4,26 +4,23 @@ public class CalculatorTest {
 	
 	public static void main(String[] args) {
 		Calculator myCalculator = new Calculator();
+		Scanner scanner = new Scanner(System.in);
 		String answer;
 
 		do {
 			System.out.println("Enter the first number: ");
-			Scanner firstNumber = new Scanner(System.in);
-			myCalculator.setNumber1(firstNumber.nextInt());
+			myCalculator.setNumber1(scanner.nextInt());
 
 			System.out.println("Enter the sign of the mathematical operation: ");
-			Scanner inputOperation = new Scanner(System.in);
-			myCalculator.setOperation(inputOperation.next().charAt(0));
+			myCalculator.setOperation(scanner.next().charAt(0));
 
 			System.out.println("Enter the second number: ");
-			Scanner secondNumber = new Scanner(System.in);
-			myCalculator.setNumber2(secondNumber.nextInt());
-			System.out.println("Total: " + myCalculator.getResult());
+			myCalculator.setNumber2(scanner.nextInt());
+			System.out.println("Total: " + myCalculator.calculation());
 
 			do {
 				System.out.println("Want to continue? [Yes/No]");
-				Scanner finish = new Scanner(System.in);
-				answer = finish.nextLine();
+				answer = scanner.next();
 			} while(!answer.equals("Yes") && !answer.equals("No"));
 
 		} while(answer.equals("Yes"));
