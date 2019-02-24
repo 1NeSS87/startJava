c35d8fe5950c9d57292f84eb2de33d6557a3af87
Nikita Sapronov <sapronov-na@ddsfirm.ru>
2019-02-25 01:30:05 +0500

    Jaeger update

--- lesson 2/JaegerTest.java
+++ lesson 2/JaegerTest.java
@@ -1,16 +1,7 @@
 public class JaegerTest {
 	
 	public static void main(String[] args) {
-		Jaeger chernoAlpha = new Jaeger();
-
-		chernoAlpha.setModelName("Cherno Alpha");
-		chernoAlpha.setMark("Mark-1");
-		chernoAlpha.setOrigin("Russia");
-		chernoAlpha.setHeight(85.34f);
-		chernoAlpha.setWeight(2.41f);
-		chernoAlpha.setSpeed(3);
-		chernoAlpha.setStrength(10);
-		chernoAlpha.setArmor(10);
+		Jaeger chernoAlpha = new Jaeger("Cherno Alpha", "Mark-1", "Russia", 85.34f, 2.41f, 3, 10, 10);
 
 		chernoAlpha.drift();
 
@@ -20,16 +11,7 @@ public class JaegerTest {
 
 		chernoAlpha.useVortexCannon();
 
-		Jaeger strikerEureka = new Jaeger();
-
-		strikerEureka.setModelName("Striker Eureka");
-		strikerEureka.setMark("Mark-5");
-		strikerEureka.setOrigin("Australia");
-		strikerEureka.setHeight(76.20f);
-		strikerEureka.setWeight(1.85f);
-		strikerEureka.setSpeed(10);
-		strikerEureka.setStrength(10);
-		strikerEureka.setArmor(9);
+		Jaeger strikerEureka = new Jaeger("Striker Eureka", "Mark-5", "Australia", 76.20f, 1.85f, 10, 10, 9);
 
 		strikerEureka.drift();
 
