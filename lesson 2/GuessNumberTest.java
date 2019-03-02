627736a05e3131bb1cc80540ee6fb33c56196d90
Nikita Sapronov <sapronov-na@ddsfirm.ru>
2019-03-03 01:05:58 +0500

    update

--- lesson 2/GuessNumberTest.java
+++ lesson 2/GuessNumberTest.java
@@ -1,26 +1,26 @@
 import java.util.Scanner;
 
 public class GuessNumberTest {
-	
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
+    
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
 
-		System.out.println("Player #1 Enter your name: ");
-		Player firstPlayer = new Player(scan.next());
+        System.out.println("Player #1 Enter your name: ");
+        Player firstPlayer = new Player(scan.next());
 
-		System.out.println("Player #2 Enter your name: ");
-		Player secondPlayer = new Player(scan.next());
+        System.out.println("Player #2 Enter your name: ");
+        Player secondPlayer = new Player(scan.next());
 
-		GuessNumber myGame = new GuessNumber(firstPlayer, secondPlayer);
-		String answer;
+        GuessNumber myGame = new GuessNumber(firstPlayer, secondPlayer);
+        String answer;
 
-		do {
-			myGame.startGame();
-			
-			do {
-				System.out.println("Want to continue? [Yes/No]");
-				answer = scan.next();
-			} while(!answer.equals("Yes") && !answer.equals("No"));
-		} while(answer.equals("Yes"));
-	}
+        do {
+            myGame.startGame();
+            
+            do {
+                System.out.println("Want to continue? [Yes/No]");
+                answer = scan.next();
+            } while(!answer.equals("Yes") && !answer.equals("No"));
+        } while(answer.equals("Yes"));
+    }
 }
